200dd68143c221cbc6a01341111b241da52f5395
Lee heechan <a_line1@naver.com>
2020-09-28 17:57:40 +0900

    reee

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "s3-webapp" {
-  source  = "github.com/beyondlimitation/terraform-aws-s3-demo"
+  source  = "github.com/beyondlimitation/terraform-aws-s3-demo?ref=v2.0.0"
   version = "2.0.0"
   name = var.name
   region = var.region
