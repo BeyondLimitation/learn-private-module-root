39804d9913b433ebd77fb2edb003fcef25bb837b
Lee heechan <a_line1@naver.com>
2020-09-28 17:13:49 +0900

    i'll do myself

--- main.tf
+++ main.tf
@@ -1,16 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
 module "s3-demo" {
   source  = "app.terraform.io/my-private-module/s3-demo/aws"
   version = "2.0.0"
+  name = var.name
+  region = var.region
+  prefix = var.prefix
 }
