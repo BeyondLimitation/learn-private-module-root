a096a303b666b76e06fd95d770dcda5cafd43721
Lee heechan <a_line1@naver.com>
2020-09-28 16:57:36 +0900

    ok..

--- main.tf
+++ main.tf
@@ -12,8 +12,5 @@ provider "aws" {
 
 module "s3-demo" {
   source  = "app.terraform.io/my-private-module/s3-demo/aws"
-  name    = var.name
-  region  = var.region
-  prefix  = var.prefix
   version = "2.0.0"
 }
