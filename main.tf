b50bf80b563d6afff50598e10e3aed782800982e
Lee heechan <a_line1@naver.com>
2020-09-28 17:54:36 +0900

    test

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "s3-webapp" {
-  source  = "app.terraform.io/my-private-module/s3-webapp/aws"
+  source  = "github.com/beyondlimitation/terraform-aws-s3-demo"
   version = "2.0.0"
   name = var.name
   region = var.region
