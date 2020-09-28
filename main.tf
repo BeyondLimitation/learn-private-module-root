31b5715c982b5e47f6b3804b5372051e3e49a6fd
Lee heechan <a_line1@naver.com>
2020-09-28 15:07:04 +0900

    test

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = var.region
 }
 
-module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
-  name        = var.name
-  region = var.region
-  prefix = var.prefix
+module "s3-demo" {
+  source  = "app.terraform.io/my-private-module/s3-demo/aws"
+  name    = var.name
+  region  = var.region
+  prefix  = var.prefix
   version = "1.0.0"
 }
