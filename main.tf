66f39ae303e7dfc36f35d4b25ac85a3f3d67807b
Lee heechan <a_line1@naver.com>
2020-09-29 13:56:51 +0900

    link was wrong

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "s3-webapp" {
-  source  = "github.com/beyondlimitation/terraform-aws-s3-demo?ref=v2.0.0"
-  version = "2.0.0"
+  source  = "app.terraform.io/my-private-module/s3-webapp/aws"
+  version = "3.0.0"
   name = var.name
   region = var.region
   prefix = var.prefix
